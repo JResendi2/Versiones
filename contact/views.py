d88dbff499ec3eac0949d3ca1a4e2ed899093e39
jose resendiz <resendizj607@gmail.com>
2024-01-13 14:17:41 -0600

    Estado antes de la funcionalidad "Crear contacto"

---
+++ contact/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from .models import Contact
+from .forms import ContactForm
+from django.contrib import messages
+
+
+def index(request):
+    contacts = Contact.objects.filter(name__contains=request.GET.get('search', ''))
+    #contacts = Contact.objects.all()
+    
+    context = {
+        'contacts': contacts
+    }
+    return render(request, 'contact/index.html', context)
+
+
+def view(request, id): 
+    contact = Contact.objects.get(id=id) 
+    context = {
+        'contact': contact
+    }
+    return render (request, 'contact/detail.html', context)
+
+def edit(request, id): 
+    contact = Contact.objects.get(id=id) 
+    
+    if(request.method == 'GET'):
+        form = ContactForm(instance = contact)
+        context = {
+            'form': form,
+            'id': id
+        }
+        return render (request, 'contact/create.html', context)
+
+    if(request.method == 'POST'):
+        form = ContactForm(request.POST, instance = contact)
+        if form.is_valid():
+            form.save()
+        context = {
+            'form': form,
+            'id': id
+        }
+        messages.success(request, "Contacto actualizado.")
+        return render(request, 'contact/create.html', context)
+
+
